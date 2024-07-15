085cebad97e4039c04ce3b29812eb33aa3921204
irmaktekin <irmaktekin97@gmail.com>
2024-07-15 16:46:26 +0300

    Endpoint is added for update and delete operations

--- src/main/java/dev/patika/ecommerce/api/CategoryController.java
+++ src/main/java/dev/patika/ecommerce/api/CategoryController.java
@@ -2,9 +2,12 @@ package dev.patika.ecommerce.api;
 
 import dev.patika.ecommerce.business.abstracts.ICategoryService;
 import dev.patika.ecommerce.core.config.modelMapper.IModelMapperService;
+import dev.patika.ecommerce.core.result.Result;
 import dev.patika.ecommerce.core.result.ResultData;
 import dev.patika.ecommerce.core.utilies.ResultHelper;
 import dev.patika.ecommerce.dto.request.category.CategorySaveRequest;
+import dev.patika.ecommerce.dto.request.category.CategoryUpdateRequest;
+import dev.patika.ecommerce.dto.response.CursorResponse;
 import dev.patika.ecommerce.dto.response.category.CategoryResponse;
 import dev.patika.ecommerce.entities.Category;
 import jakarta.validation.Valid;
@@ -44,14 +47,31 @@ public class CategoryController {
 
     @GetMapping()
     @ResponseStatus(HttpStatus.OK)
-    public Page<CategoryResponse> cursor(
+    public ResultData<CursorResponse<CategoryResponse>> cursor(
             @RequestParam(name = "page",required = false,defaultValue = "0") int page,
             @RequestParam(name = "pageSize", required = false, defaultValue = "10") int pageSize
     ){
       Page<Category> categoryPage = this.iCategoryService.cursor(page,pageSize);
       Page<CategoryResponse> categoryResponsePage = categoryPage
               .map(category -> this.modelMapper.forResponse().map(category,CategoryResponse.class));
-      return categoryResponsePage;
+      return ResultHelper.cursor(categoryResponsePage);
     }
+
+    @PutMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<CategoryResponse> update(@Valid @RequestBody CategoryUpdateRequest categoryUpdateRequest){
+        Category updateCategory = this.modelMapper.forRequest().map(categoryUpdateRequest,Category.class);
+        this.iCategoryService.update(updateCategory);
+        return ResultHelper.success(this.modelMapper.forResponse().map(updateCategory,CategoryResponse.class));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Result delete(@PathVariable("id") int id){
+        this.iCategoryService.delete(id);
+        return ResultHelper.ok();
+    }
+
+
 
 }
