a33a9b6367c8b7db5580f5a43c193c5697d5fa20
irmaktekin <irmaktekin97@gmail.com>
2024-07-15 15:10:14 +0300

    Messages are created for responses

---
+++ src/main/java/dev/patika/ecommerce/core/utilies/Message.java
@@ -0,0 +1,8 @@
+package dev.patika.ecommerce.core.utilies;
+
+public class Message {
+    public static final String CREATED = "Data is created";
+    public static final String VALIDATE_ERROR = "Not valid data.";
+
+
+}
